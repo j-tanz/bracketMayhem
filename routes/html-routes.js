8ce975dea4154181305f21b24fb0148c0e4a262e
laurencantlin <laurenrose5321@gmail.com>
2018-03-06 14:01:31 -0600

    adds package.json file and html route

---
+++ routes/html-routes.js
@@ -0,0 +1,11 @@
+
+var path = require("path");
+
+
+module.exports = function(app) {
+
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/homepage.html"));
+  });
+
+};
