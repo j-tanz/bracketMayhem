8b21ec31f0e5f65365b569ce36992c28142420fa
laurencantlin <laurenrose5321@gmail.com>
2018-03-07 14:47:28 -0600

    html for login and signup page and start of tests, and html routes for pages

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
-// var sequelize = require("sequelize")
 var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -10,13 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
-// require("./models/liveResult.js")(app);
 
 require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
 
-// db.sequelize.sync({ force: true }).then(function() {
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
-// });
