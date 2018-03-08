010c0cc224ec25619e6262d9fc51500fc37c04f8
laurencantlin <laurenrose5321@gmail.com>
2018-03-08 11:58:09 -0600

    fixes check if user email already exists

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,16 @@
 function loginTo(emailInput, passInput) {
 }
-
+function correctPassword(email, pw) {
+    var queryURL = "/api/logins/" + email;
+    $.get(queryURL, function (data) {
+        console.log(data.loginPassword);
+        if (data.loginPassword !== pw) {
+            console.log("bad pw");
+            return true
+        }
+        console.log("yay pw correct");
+    })
+}
 // Get user inputs for pw and email
 //check if  inputemail exists in database
     //if check return true, return correct password
