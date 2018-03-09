4336cbb9ce7b0921edeea5b13444e1e0216258ec
laurencantlin <laurenrose5321@gmail.com>
2018-03-09 14:39:16 -0600

    creates get request to userBracket table if password is correct

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,29 @@
 function loginTo(emailInput, passInput) {
 }
-function correctPassword(email, pw) {
-    var queryURL = "/api/logins/" + email;
-    $.get(queryURL, function (data) {
+function correctPassword(evt) {
+    evt.preventDefault();
+    var inputEmail = $("#loginInputEmail1").val().trim();
+    var inputPass = $("#loginInputPassword1").val().trim();
+    console.log(inputEmail, inputPass);
+    var loginQueryURL = "/api/logins/" + inputEmail;
+    $.get(loginQueryURL, function (data) {
         console.log(data.loginPassword);
-        if (data.loginPassword !== pw) {
+        if (data.loginPassword !== inputPass) {
             console.log("bad pw");
             return true
         }
         console.log("yay pw correct");
+        var userBracketQueryURL = "/api/userBrackets/" + data.id;
+        $.get(userBracketQueryURL, function(bracketdata){
+            console.log(bracketdata);
+        })
+
     })
 }
+
+$(".signin-btn").click(correctPassword);
+
+
 // Get user inputs for pw and email
 //check if  inputemail exists in database
     //if check return true, return correct password
